293d6d624de48afcdb712ec6231a40ab597bc576
congowar <congowar@gmail.com>
2016-01-13 18:15:47 +0200

    update react app

--- js/main.js
+++ js/main.js
@@ -1,16 +1,94 @@
 $(function() {
     var app_id = '552216338261451';
-    var user_id = '';
-    var postsLimit = 10;
     var scopes = 'email, user_friends, public_profile';
     var postData = [];
+    var userData = {};
+    var postsLimit = 5;
 
-    var btn_login = '<a href="#" id="login" class="btn btn-primary">Log in with Facebook</a>';
-    var div_session = '<div id="facebook-session">'+
-                      '<strong></strong><br>'+
-                      '<img class="avatar"><br>'+
-                      '<a href="#" id="logout" class="btn btn-danger">LOGOUT</a>'+
-                      '</div>';
+
+    var FacebookUserLogin = React.createClass({
+        render: function() {
+            var user = this.props.data;
+            return (
+                <div className="login-wrapper">
+                    <div className="logo">
+                        <img src="img/reactjs.png" alt="logo" width="54px"/>
+                    </div>
+                    <p className="header-text">Facebook Application</p>
+                    <div className="wrapper-block">
+                        <a href="#" id="logout" className="btn btn-danger">LOGOUT</a>
+                        <div className="user-avatar"><img src={user.avatar}/></div>
+                        <div className="wrapper-name">
+                            <p className="user-data"><strong>Name: </strong>
+                                <span className="user-info">{user.name}</span>
+                            </p>
+                            <p className="user-data"><strong>Email: </strong>
+                                <span className="user-info">{user.email}</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="clr"></div>
+                </div>
+            );
+        }
+    });
+
+    var FacebookPost = React.createClass({
+        render: function() {
+            var shortLink = '';
+
+            return (
+                <div className="post-wrapper">
+                    {this.props.data.map(function(current, index) {
+
+                        shortLink = current.link.slice(0, 40) + "...";
+
+                        return <div className="post-block">
+                            <h2 className="post-header">{current.header}</h2>
+
+                            <div className="parent-wrapper">
+                                <img src={'http://graph.facebook.com/' + current.pageId + '/picture?type=normal'} width="60px"/>
+                                <p className="post-info"><strong>Name: </strong>{current.name}</p>
+                                <p className="post-info"><strong>Link: </strong><a href={current.link} target="_blank">{shortLink}</a></p>
+                                <p className="post-description">
+                                    <img src={current.picture} width="40%" />
+                                    {current.description}
+                                </p>
+                            </div>
+                            <div className="separator"></div>
+                        </div>
+                    })}
+                </div>
+            );
+        }
+    });
+
+    var LoginButton = React.createClass({
+        render: function() {
+            return (
+                <a href="#" id="login" className="btn btn-primary ">
+                    <i className="fa fa-facebook-official fa-2x"></i>
+                    <span className="login-text">Log in with Facebook</span>
+                </a>
+            );
+        }
+    });
+
+    ReactDOM.render(<LoginButton />, document.getElementById('button-container'));
+
+    var DownloadButton = React.createClass({
+        getNewPosts: function() {
+            postsLimit += 5;
+            getFacebookData();
+            console.log("Current posts: " + postsLimit);
+        },
+
+        render: function() {
+            return (
+                <div onClick={this.getNewPosts} id="downloadmore" className="btn">Load More</div>
+            );
+        }
+    });
 
     window.fbAsyncInit = function() {
         FB.init({
@@ -28,10 +106,9 @@ $(function() {
     };
 
     var statusChangeCallback = function(response, callback) {
-        console.log(response);
         if (response.status === 'connected') {
             getFacebookData();
-            getPosts();
+            $('#login').remove();
         } else {
             callback(false);
         };
@@ -40,66 +117,65 @@ $(function() {
     var checkLoginState = function(callback) {
         FB.getLoginStatus(function(response) {
             statusChangeCallback(response, function(data) {
-                console.log(data);
                 callback(data);
             });
         });
     };
 
     var getFacebookData = function() {
-        FB.api('/me', function(response) {
-            $('#login').after(div_session);
-            $('#login').remove();
-            $('#facebook-session strong').text("Name: " + response.name);
-            $('#facebook-session').text(response.link);
-            $('#facebook-session img').attr('src', 'http://graph.facebook.com/' + response.id + '/picture?type=large');
-        });
-    };
-
-    var getPosts = function() {
-        FB.api('/me', 'GET', {"fields": "posts.limit(" + postsLimit + "){full_picture, name, caption, description, icon}"},
+        FB.api('/me', 'GET',
+            {"fields": "id, name, email, posts.limit(" + postsLimit + "){full_picture, name, description, link, parent_id}"},
             function(response) {
+
             var posts = response.posts.data;
 
-            for (var i = 0; i < posts.length; i++) {
+            userData.id = response.id;
+            userData.email = response.email;
+            userData.name = response.name;
+            userData.avatar = 'http://graph.facebook.com/' + response.id + '/picture?type=small';
+
+            for (var i = postsLimit - 5; i < posts.length; i++) {
                 postData.push({
                     name: posts[i].name,
                     description: posts[i].description,
                     picture: posts[i].full_picture,
-                    caption: posts[i].caption,
-                    icon: posts[i].icon
+                    link: posts[i].link,
+                    parentId: posts[i].parent_id,
+
+                    //get post-header from description
+                    get header() {
+                        var header = [];
+                        if (this.description)
+                            header.push(this.description.split('.', 1));
+                        else
+                            header.push(this.name);
+                        return header;
+                    },
+
+                    get pageId() {
+                        var id = [];
+                        if (this.parentId)
+                            id.push(this.parentId.split('_', 1));
+                        return id;
+                    }
                 });
             };
-        });
 
-        var FacebookPost = React.createClass({
-            render: function() {
-                return (
-                    <div>
-                        {this.props.data.map(function(current, index) {
-                            return <div className="post-block">
-                                      <h2 className="post-header">Header</h2>
-                                      <p>From: {current.name}</p><img src={current.icon} />
-                                      <img src={current.picture} width="100%" />
-                                      <p className="post-description">{current.description}</p>;
-                                      <div className="separator"></div>
-                                   </div>;
-                        })}
-                    </div>
-                )
-            }
+            console.log(postData);
+            ReactDOM.render(<FacebookUserLogin data={userData} />, document.getElementById('facebook-login'));
+            ReactDOM.render(<FacebookPost data={postData} />, document.getElementById('facebook-post'));
+            ReactDOM.render(<DownloadButton />, document.getElementById('button-dwnd-container'));
         });
-
-        ReactDOM.render(<FacebookPost data={postData} />, document.getElementById('facebook-post'));
     };
 
     var facebookLogin = function() {
         checkLoginState(function(response) {
             if (!response) {
                 FB.login(function(response) {
-                    if (response.status === 'connected')
-                    getFacebookData();
-                    getPosts();
+                    if (response.status === 'connected') {
+                        getFacebookData();
+                        $('#login').remove();
+                    }
                 }, {scope: scopes});
             };
         });
@@ -109,8 +185,11 @@ $(function() {
         FB.getLoginStatus(function(response) {
             if (response.status === 'connected') {
                 FB.logout(function(responce) {
-                    $('#facebook-session').before(btn_login);
-                    $('#facebook-session').remove();
+                    $('#facebook-login').empty();
+                    $('#facebook-post').empty();
+                    $('#button-dwnd-container').empty();
+                    $('#logout').remove();
+                    ReactDOM.render(<LoginButton />, document.getElementById('button-container'));
                 })
             };
         });
@@ -120,6 +199,7 @@ $(function() {
     $(document).on('click', '#login', function(e) {
         e.preventDefault();
         facebookLogin();
+        //location.reload();
     });
 
     $(document).on('click', '#logout', function(e) {
